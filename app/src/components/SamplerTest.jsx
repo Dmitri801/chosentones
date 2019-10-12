d187d0f12b2e3a2daa470aabad33d98618d5227e
Dmitri801 <dmitriys801@gmail.com>
2019-10-12 14:14:51 -0600

    basic tonejs setup with hooks

---
+++ app/src/components/SamplerTest.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Sampler } from "tone";
+import A1 from "../assets/testAudio/Lex808.mp3";
+function SamplerTest() {
+    const [isLoaded, setLoaded] = React.useState(false);
+    const sampler = React.useRef(
+        new Sampler(
+            { A1 },
+            {
+                onload: () => {
+                    setLoaded(true);
+                }
+            }
+        ).toMaster()
+    );
+
+    const handleClick = () => sampler.current.triggerAttack("A1");
+    return (
+        <div>
+            <h1> here we'll test the sampler</h1>
+            <button disabled={!isLoaded} onClick={handleClick}>
+                start
+      </button>
+        </div>
+    )
+}
+
+export default SamplerTest;
